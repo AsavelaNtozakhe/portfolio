1b94bfc5959495820f4b7c4ac3c050ad530f42f1
AsavelaNtozakhe <131442652+AsavelaNtozakhe@users.noreply.github.com>
2023-08-18 06:58:07 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Software Developer", "Front-end Developer", "JavaScri pt wizard"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
